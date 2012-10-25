9a05a377e1d4f5dcb76dc3f408f62243d3b79434
Daniel Micay <danielmicay@gmail.com>
2012-10-25 00:59:33 -0400

    either.hh: add copy and move constructors

--- either.hh
+++ either.hh
@@ -8,6 +8,22 @@ struct either {
   either(Left &&other) : left(std::move(other)), is_left(true) {}
   either(Right &&other) : right(std::move(other)), is_left(false) {}
 
+  either(const either &other) : is_left(other.is_left) {
+    if (is_left) {
+      new(&left) Left(other.left);
+    } else {
+      new(&right) Right(other.right);
+    }
+  }
+
+  either(either &&other) : is_left(other.is_left) {
+    if (is_left) {
+      new(&left) Left(std::move(other.left));
+    } else {
+      new(&right) Right(std::move(other.right));
+    }
+  }
+
   template<typename Visitor>
   void apply_visitor(Visitor visitor) {
     if (is_left) {
